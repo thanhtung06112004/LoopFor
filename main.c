cbbdd615b910a933f2b317f13b28af5714efa303
thanhtung06112004 <145945848+thanhtung06112004@users.noreply.github.com>
2023-10-30 09:22:12 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+//    viet chuong trinh tinh tong 1 den 100
+//    int i, tong = 0;
+//    for (i = 1; i < 100 ; i++) {
+//        tong += 1;
+//    }
+//    printf("%d\n", tong);
+    
+    //    viet chuong trinh vao 1 tu nhien n. tinh tong cac so chia het cho 3 be hon n.
+//    int n;
+//    printf("Nhap n: ");
+//    scanf("%d", &n);
+//        int i, tong = 0;
+//        for (i = 1; i < 100 ; i++) {
+//            if(i % 3 == 0);
+//            tong += i;
+//        }
+//        printf("%d\n", tong);
+    
+    //    in ra 100 so fibonac
+    
+//    int n1 = 0;
+//    int n2 = 1;
+//    int n3 = 1;
+//    int i;
+//    for (i = 10; i < 100; i++) {
+//        printf("%d ", n3);
+//        n3 = n1 + n2;
+//        n1 = n2;
+//        n2 = n3;
+//    }
+
+    return 0;
+}
